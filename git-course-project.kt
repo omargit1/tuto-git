3546663bf4488e86f87dc04879ae1f3c6868e0f6
EL-ASSRI Omar (Consulting For ACCOR Hotels) <Omar.EL-ASSRI@consulting-for.accor.com>
2022-02-20 13:36:01 +0100

    add third parameter and comment

--- git-course-project.kt
+++ git-course-project.kt
@@ -1,4 +1,5 @@
-fun addNumbers(num1 : Int, num2 : Int): Unit {
-    print("${num1 + num2}")
+fun addNumbers(num1 : Int, num2 : Int, num3: Int): Unit {
+	// add the three parameters
+    print("${num1 + num2 + num3}")
 }
 
